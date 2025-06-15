c111b16961593fcfb4458010c783e9e462564f42
slepoi_kamin <aleulyanov@gmail.com>
2025-06-15 20:24:41 +0800

    feat: added install_mac.sh and new clean .zshrc

---
+++ scripts/install_mac.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+echo "🔧 Setting up..."
+
+# --- Install homebrew ---
+if [[ ! -f "/opt/homebrew/bin/brew" ]] then
+  echo "➡️  Installing Homebrew..."
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "✅ Homebrew has already been installed."
+fi
+
+# --- Install packages ---
+echo "➡️  Installing required packages ..."
+
+echo "➡️  Installing stow ..."
+brew install stow
+
+echo "➡️  Installing fonts ..."
+brew install --cask font-fira-code-nerd-font font-jetbrains-mono-nerd-font
+
